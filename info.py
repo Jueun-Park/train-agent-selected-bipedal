2b741182a9153f1ae56f9a2c4971010b13d206e7
jueun-park <elpion19@gmail.com>
2020-04-14 19:39:46 +0900

    init commit

---
+++ info.py
@@ -0,0 +1,8 @@
+mode_dict = {
+    0: "grass",
+    1: "stump",
+    2: "stairs",
+    3: "pit",
+}
+total_timesteps = 50000000  # 50M
+num_cpu = 64
---
+++ train.py
@@ -0,0 +1,52 @@
+import argparse
+from pathlib import Path
+import tensorflow as tf
+import gym
+import gym_selected_bipedal
+
+from stable_baselines import ACKTR
+from stable_baselines.common.policies import MlpPolicy
+from stable_baselines.common.vec_env import SubprocVecEnv, VecEnvWrapper
+from vec_monitor import VecMonitor
+# from stable_baselines.common import make_vec_env
+from stable_baselines.bench import Monitor
+
+from info import mode_dict, total_timesteps, num_cpu
+from save_callback import SaveOnBestTrainingRewardCallback
+from make_env import make_env
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--base-index", type=int)
+args = parser.parse_args()
+base_index = args.base_index
+
+algo = "ACKTR"
+log_dir = f"{str(algo)}/{mode_dict[base_index]}"
+Path(log_dir).mkdir(parents=True, exist_ok=True)
+
+if __name__ == "__main__":
+    env_id = f"selected-bipedal-{mode_dict[base_index]}-v0"
+    env = SubprocVecEnv([make_env(env_id, i) for i in range(num_cpu)])
+    env = VecMonitor(env, filename=log_dir)
+
+    callback = SaveOnBestTrainingRewardCallback(
+        check_freq=1000, log_dir=log_dir)
+
+    model = ACKTR(
+        MlpPolicy,
+        env,
+        verbose=1,
+        tensorboard_log=f"./tensorboard_{algo}/",
+        policy_kwargs={
+            'layers': [64, 256, 256, 64],
+            'act_fun': tf.nn.relu
+            }
+        )
+
+    model.learn(
+        total_timesteps=total_timesteps,
+        callback=callback,
+        )
+
+    model.save(log_dir)
